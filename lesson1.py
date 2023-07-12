f6b8ece11b668d1c8897a73ee5ee07d999f2f815
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-07-12 23:54:16 +0300

    Update lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -1,5 +1,6 @@
 name = "Islam "
 
+
 # print(len(name)) # длина всей строки
 # print(name.find("o")) # ишет под каким индексом находится буква o
 # print(name.capitalize()) # Делает первую букву заглавной
