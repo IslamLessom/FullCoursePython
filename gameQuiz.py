65b5e646052f434c525e0a5d278e61827f9cedda
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-07-30 13:38:01 +0300

    Add files via upload

---
+++ gameQuiz.py
@@ -0,0 +1,17 @@
+def new_game():
+    pass
+def check_answer():
+    pass
+def display_score():
+    pass
+def play_again():
+    pass
+
+questions = {
+    "Who created Python?: " : "A",
+    "What year was Python created?: " : "B",
+    "Python is tributed to whicj comedy group?: " : "C",
+    "Is the Earcth round?: " : "A",
+}
+
+options = [["A. "]]
